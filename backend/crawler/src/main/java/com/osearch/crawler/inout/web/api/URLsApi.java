3f5fa75292061b34cb7f1c2f49ecd5dc0bf48a36
Alexander Tolpeko <alextolpeko@gmail.com>
2023-05-25 09:34:23 +0200

    Crawler: designed urls API

---
+++ backend/crawler/src/main/java/com/osearch/crawler/inout/web/api/URLsApi.java
@@ -0,0 +1,18 @@
+package com.osearch.crawler.inout.web.api;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(path = "/urls", produces = "application/json")
+@Api(tags = "URLs API")
+@Validated
+public interface URLsApi {
+
+    @GetMapping("/count")
+    @ApiOperation(value = "Get processed URLs count")
+    Long count();
+}
