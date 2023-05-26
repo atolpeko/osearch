c08a6f3881a47dee40c00519879b153168f5c106
Alexander Tolpeko <alextolpeko@gmail.com>
2023-05-26 22:57:05 +0200

    Crawler: configuration fix

--- backend/crawler/src/main/java/com/osearch/crawler/config/KafkaConfig.java
+++ backend/crawler/src/main/java/com/osearch/crawler/config/KafkaConfig.java
@@ -12,11 +12,9 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 
 @Configuration
 @RequiredArgsConstructor
@@ -26,13 +24,11 @@ public class KafkaConfig {
     private KafkaProperties kafkaProperties;
 
     @Bean
-    @Profile({"local", "prod"})
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
-    @Profile({"local", "prod"})
     public ProducerFactory<String, String> producerFactory() {
         var configProps = new HashMap<String, Object>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getUrl());
@@ -40,21 +36,5 @@ public class KafkaConfig {
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    @Profile({"debug", "test"})
-    public EmbeddedKafkaBroker embeddedKafkaBroker() {
-        return new EmbeddedKafkaBroker(1, true, kafkaProperties.getUrlTopic(),
-                kafkaProperties.getRequestTopic(), kafkaProperties.getResponseTopic());
-    }
-
-    @Bean
-    @Profile({"debug", "test"})
-    public ProducerFactory<String, String> producerFactory(EmbeddedKafkaBroker embeddedKafkaBroker) {
-        var config = new HashMap<String, Object>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                embeddedKafkaBroker.getBrokersAsString());
-        return new DefaultKafkaProducerFactory<>(config);
     }
 }
---
+++ backend/crawler/src/test/java/com/osearch/crawler/config/EmbeddedKafkaConfig.java
@@ -0,0 +1,38 @@
+package com.osearch.crawler.config;
+
+import com.osearch.crawler.config.properties.KafkaProperties;
+
+import java.util.HashMap;
+
+import lombok.RequiredArgsConstructor;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+
+@Configuration
+@RequiredArgsConstructor
+public class EmbeddedKafkaConfig {
+
+    @Autowired
+    private KafkaProperties kafkaProperties;
+
+    @Bean
+    public EmbeddedKafkaBroker embeddedKafkaBroker() {
+        return new EmbeddedKafkaBroker(1, true, kafkaProperties.getUrlTopic(),
+                kafkaProperties.getRequestTopic(), kafkaProperties.getResponseTopic());
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(EmbeddedKafkaBroker embeddedKafkaBroker) {
+        var config = new HashMap<String, Object>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                embeddedKafkaBroker.getBrokersAsString());
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+}
