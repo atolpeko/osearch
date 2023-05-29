00b463a3ec50707a5edee641e5cbff569d90136b
Alexander Tolpeko <alextolpeko@gmail.com>
2023-05-29 09:18:40 +0200

    Indexer: kafka listener

---
+++ backend/indexer/src/main/java/com/osearch/indexer/inout/messaging/consumer/RequestMessageListener.java
@@ -0,0 +1,60 @@
+package com.osearch.indexer.inout.messaging.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.osearch.indexer.config.properties.KafkaProperties;
+import com.osearch.indexer.inout.messaging.entity.NewUrlRequest;
+
+import javax.validation.Validator;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class RequestMessageListener {
+    private final ObjectMapper objectMapper;
+    private final Validator validator;
+
+    private final KafkaProperties properties;
+
+    @KafkaListener(
+            topics = "#{kafkaProperties.getUrlTopic()}",
+            groupId = "#{kafkaProperties.getGroupId()}"
+    )
+    public void listen(String message) {
+        try {
+            log.debug("Receiving a message from topic {}: {} ",
+                    properties.getRequestTopic(), message);
+            var request = toRequest(message);
+            process(request);
+        } catch (JsonMappingException e) {
+            log.error("{} doesn't match the structure of NewUrlRequest", message);
+        } catch (JsonProcessingException e) {
+            log.error("{} is not a valid JSON", message);
+        } catch (IllegalArgumentException e) {
+            log.debug("{} is not a valid NewUrlRequest", message);
+        } catch (Exception e) {
+            log.error("Error: {}", e.getMessage());
+        }
+    }
+
+    private NewUrlRequest toRequest(String message) throws JsonProcessingException {
+        var request = objectMapper.readValue(message, NewUrlRequest.class);
+        if (!validator.validate(request).isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        return request;
+    }
+
+    private void process(NewUrlRequest request) {
+        // service call
+    }
+}
