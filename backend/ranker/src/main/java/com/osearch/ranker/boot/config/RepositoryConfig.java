1f90e1c48f2a57eae00374f5d3e766a79490f14b
Alexander Tolpeko <alextolpeko@gmail.com>
2023-09-17 13:38:21 +0200

    Ranker: repository configuration

---
+++ backend/ranker/src/main/java/com/osearch/ranker/boot/config/RepositoryConfig.java
@@ -0,0 +1,30 @@
+package com.osearch.ranker.boot.config;
+
+import com.osearch.ranker.adapter.out.IndexRepositoryImpl;
+import com.osearch.ranker.adapter.out.PageRepositoryImpl;
+import com.osearch.ranker.adapter.out.jpa.IndexDtoJpaRepository;
+import com.osearch.ranker.adapter.out.mapper.IndexMapper;
+import com.osearch.ranker.application.port.IndexRepository;
+import com.osearch.ranker.application.port.PageRepository;
+
+import org.neo4j.driver.Driver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RepositoryConfig {
+
+    @Bean
+    public PageRepository pageRepository(Driver driver) {
+        return new PageRepositoryImpl(driver);
+    }
+
+    @Bean
+    public IndexRepository indexRepository(
+        IndexDtoJpaRepository jpaRepository,
+        IndexMapper mapper
+    ) {
+        return new IndexRepositoryImpl(jpaRepository, mapper);
+    }
+}
