050b11eb86146f508082933b6e5029bda59255b9
Alexander Tolpeko <alextolpeko@gmail.com>
2023-09-19 15:58:41 +0200

    Crawler: Changed hashing algorithm from MD5 to SHA256

--- backend/crawler/src/test/java/com/osearch/crawler/fixture/HasherFixture.java
+++ backend/crawler/src/test/java/com/osearch/crawler/fixture/HasherFixture.java
@@ -2,5 +2,6 @@ package com.osearch.crawler.fixture;
 
 public class HasherFixture {
     public static final String VALUE = "dffdfdfdf";
-    public static final String HASH = "5f2c65e7a5dbb72d7a2e3b847057aafe";
+    public static final String HASH =
+        "ecd9f9ca257e5622694f2cba31233782c87e6533d8c205e94c311e2cb1b38a98";
 }
