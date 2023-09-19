581480a70b9aedfa178ed176445bbfbc6d632fbb
Alexander Tolpeko <alextolpeko@gmail.com>
2023-09-19 17:30:57 +0200

    Crawler: fixed kafka request mapping

--- backend/crawler/src/main/java/com/osearch/crawler/adapter/in/messaging/entity/Request.java
+++ backend/crawler/src/main/java/com/osearch/crawler/adapter/in/messaging/entity/Request.java
@@ -3,14 +3,18 @@ package com.osearch.crawler.adapter.in.messaging.entity;
 import java.io.Serializable;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents a request object used to interact with crawler service via Kafka.
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Request implements Serializable {
 
     /**
